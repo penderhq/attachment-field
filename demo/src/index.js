001f01f32ce5990ce093cd51fcb2e1af52ec342c
Andev <olivier@andev.nl>
2018-11-06 16:44:49 +0700

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import AttachmentField from '../../src'
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>attachment-field Demo</h1>
-      <Example/>
-    </div>
-  }
+class Demo extends React.Component {
+    render() {
+        return <div>
+            <h1>AttachmentField</h1>
+            <AttachmentField
+                mode={'preview'}
+                attachments={[{
+                    id: '1',
+                    filename: 'Attachment 1',
+                    url: 'https://placekitten.com/200/300'
+                }, {
+                    id: '2',
+                    filename: 'Attachment 2',
+                    url: 'https://placekitten.com/200/300'
+                }, {
+                    id: '2',
+                    filename: 'Attachment 1',
+                    url: 'https://placekitten.com/200/300'
+                }]}
+            />
+        </div>
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css} from 'emotion'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+class Attachment extends React.Component {
+
+    render() {
+
+        return (
+            <div
+                className={css`
+                    width: 200px;
+                    height: 200px;
+                    background-image: url(${this.props.attachment.url});
+                    background-size: cover;
+                `}
+            >
+                empty
+            </div>
+        )
+    }
+}
+
+export default class AttachmentField extends React.Component {
+    render() {
+        return (
+            <div
+                className={css`
+                    display: flex;
+                `}
+            >
+                {this.props.attachments.map(attachment => (
+                    <div
+                        key={attachment.id}
+                        className={css`
+                            margin-right: 6px;
+                        `}
+                    >
+                        <Attachment
+                            attachment={attachment}
+                        />
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
